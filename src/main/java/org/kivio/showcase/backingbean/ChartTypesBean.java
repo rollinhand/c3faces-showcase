f715ed37fe9e1cec6bff05b1048f8dd487c23b88
Björn Berg <rollin.hand@gmx.de>
2018-03-27 20:03:56 +0200

    introducing C3Colors and C3Theme

--- src/main/java/org/kivio/showcase/backingbean/ChartTypesBean.java
+++ src/main/java/org/kivio/showcase/backingbean/ChartTypesBean.java
@@ -3,6 +3,8 @@ package org.kivio.showcase.backingbean;
 import org.kivio.c3faces.model.C3DataSet;
 import org.kivio.c3faces.model.C3ViewDataSet;
 import org.kivio.c3faces.script.property.Data;
+import org.kivio.c3faces.style.C3Theme;
+
 import java.io.Serializable;
 import java.util.Arrays;
 import javax.annotation.PostConstruct;
@@ -22,13 +24,17 @@ public class ChartTypesBean implements Serializable {
     @PostConstruct
     public void init() {
         data.getDataSets().add(new C3ViewDataSet("Data sample 1",
-                new C3DataSet(Arrays.asList(22, 91, 158, 93, 201, 11)), "#1060E0"));
+                new C3DataSet(Arrays.asList(22, 91, 158, 93, 201, 11)),
+                C3Theme.WARM_BLUE.getColor(0)));
         data.getDataSets().add(new C3ViewDataSet("Data sample 2",
-                new C3DataSet(Arrays.asList(1, 72, 23, 33, 21)), "#16525E"));
+                new C3DataSet(Arrays.asList(1, 72, 23, 33, 21)),
+                C3Theme.WARM_BLUE.getColor(1)));
         data.getDataSets().add(new C3ViewDataSet("Data sample 3",
-                new C3DataSet(Arrays.asList(45, 231, 2, 151, 341, 254)), "#0792AD"));
+                new C3DataSet(Arrays.asList(45, 231, 2, 151, 341, 254)),
+                C3Theme.WARM_BLUE.getColor(2)));
         data.getDataSets().add(new C3ViewDataSet("Data sample 4",
-                new C3DataSet(Arrays.asList(88, 13, 258, 211, 151)), "#8FE2F2"));
+                new C3DataSet(Arrays.asList(88, 13, 258, 211, 151)),
+                C3Theme.WARM_BLUE.getColor(3)));
 
     }
 
